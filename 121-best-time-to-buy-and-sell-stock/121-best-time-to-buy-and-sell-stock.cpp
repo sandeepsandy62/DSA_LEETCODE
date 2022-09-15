8447248bc3fbe806262f6129f4681975030c263b
Gogarla Sandeep <70258063+sandeepsandy62@users.noreply.github.com>
2022-09-16 00:11:33 +0530

    Time: 277 ms (18.25%), Space: 93.3 MB (51.54%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        
+        //APPROACH 1 
+        
         /*Brute force O(N^2) will not work TLE 
         int n = prices.size();
         int cur , mx = INT_MIN;
@@ -15,6 +18,16 @@ public:
         if(mx == INT_MIN || mx <= 0) return 0;
         return mx ;
         */
+        
+        //APPROACH 2
+        
+        //variation of next greater element problem pattern
+        //Here we need to add one extra condition to check if the
+        //price of the current day stock is less than nexGreater[i] value
+        //if it is true we will update it by nextgreater[i+1]
+        //By this we can maintain greatest stock available in the array
+        
+        /*TC : O(N) , SC : O(N)
         
         int n = prices.size();
         vector<int>nxtGreater(n,0);
@@ -36,5 +49,18 @@ public:
         }
         
         return mx;
+        */
+        
+        //APPROACH 3
+        //find minimum element and find maximum profit with that minimum element
+        //TC : O(N) , SC : O(1)
+        int n = prices.size();
+        int mn = INT_MAX , mx = 0;
+        for(int i = 0 ; i < n;i++){
+            mn = min(mn,prices[i]);
+            mx = max(mx,prices[i]-mn);
+        }
+        return mx;
+        
     }
 };
