909ad8deb5ca020f817ebd5a05af14530012d9cd
Gogarla Sandeep <70258063+sandeepsandy62@users.noreply.github.com>
2022-09-18 09:47:18 +0530

    Time: 3 ms (73.16%), Space: 5.8 MB (73.64%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -9,8 +9,24 @@ public:
         //so when ever our y^2 <= x condition failed then , we find our ans which is 1 less than y 
         //TC : O(SQRT(X)) , SC : O(1)
         
-        long long int y = 0 ;
-        while(y*y <= x) y++;
-        return y-1;
+        // long long int y = 0 ;
+        // while(y*y <= x) y++;
+        // return y-1;
+        
+        
+        //Now actualy what we are doing we are checking from 1 , 2 ,3 ,.....,sqrt(x)
+        //instead of doing linear search we use binary search so that time complexity will be O(logy)
+        //as we know ans will be in range when y*y <= x , so we store ans in mid*mid <= x condition
+        //Given constraints 0 <= x <= 2^31 - 1
+        //that is the reason we are storing low = 0 , high - INT_MAX
+        
+        long long low = 0 , high = INT_MAX ,ans = 0;
+        while(low <= high){
+            long long mid = low + (high-low)/2 ;
+            if(mid*mid <= x) ans = mid , low = mid+1;
+            else high = mid -1 ;
+        }
+        
+        return ans;
     }
 };
