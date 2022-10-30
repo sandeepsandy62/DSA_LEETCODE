ef4bd85de2e196c40cff63158d46871a7d966992
Gogarla Sandeep <70258063+sandeepsandy62@users.noreply.github.com>
2022-10-30 12:39:55 +0530

    Time: 1121 ms (53.01%), Space: 15.5 MB (75.05%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], H: int) -> int:
+        
+        def feasible(speed) -> bool:
+            return sum(math.ceil(pile/speed) for pile in piles) <= H
+        
+        left , right = 1 , max(piles)
+        while left < right:
+            mid = left + (right-left)//2 
+            if feasible(mid):
+                right = mid
+            else:
+                left = mid+1
+        return left
